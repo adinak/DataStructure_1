53f8d302888cf2cae86b5104e89f508b4280c7ab
Adina Katz <48714000+adinak@users.noreply.github.com>
2020-05-07 22:33:49 +0300

    started StreamingChart | | | to continue: need song and artist structs for creating Tree pointers

---
+++ StreamingChart/streamingChartNode.h
@@ -0,0 +1,16 @@
+//
+// Created by Adina Katz on 07/05/2020.
+//
+
+#ifndef WET1_STREAMINGCHARTNODE_H
+#define WET1_STREAMINGCHARTNODE_H
+
+#include "AVLTree.h"
+
+typedef struct StreamingChartNode{
+    int numberOfStreams;
+
+} ChartNode;
+
+
+#endif //WET1_STREAMINGCHARTNODE_H
